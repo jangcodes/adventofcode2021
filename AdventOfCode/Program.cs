ce22583a6bb31c05de607349ef0ce9cc10d2a168
jangcodes <76521944+jangcodes@users.noreply.github.com>
2021-12-02 19:38:46 -0500

    Initial Day 1

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,18 @@
+﻿string[]? textLine = await System.IO.File.ReadAllLinesAsync(@"PuzzleInput.txt");
+int[]? numberLines = textLine.Select(textLine => Convert.ToInt32(textLine)).ToArray();
+
+int increasedCount = 0;
+
+for (int i = 3; i < textLine.Length; i++)
+{
+    int previousSum = numberLines[i - 1] + numberLines[i - 2] + numberLines[i - 3];
+    int currentSum = numberLines[i] + numberLines[i - 1] + numberLines[i - 2];
+
+    if (currentSum > previousSum)
+    {
+        increasedCount++;
+    }
+}
+
+Console.WriteLine(increasedCount);
+Console.ReadLine();
