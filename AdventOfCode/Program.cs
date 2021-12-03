07bdfca2b43946236c098c16419eaca1c6e25ea6
jangcodes <jacob.jang@outlook.com>
2021-12-02 20:18:50 -0500

    First Part of Day 2

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,9 +1,13 @@
-﻿string[]? textLine = await System.IO.File.ReadAllLinesAsync(@"Day1Input.txt");
-int[]? numberLines = textLine.Select(textLine => Convert.ToInt32(textLine)).ToArray();
+﻿Console.WriteLine("================================");
+Console.WriteLine("=============Day 1==============");
+Console.WriteLine("================================");
+
+string[]? textLineDay1 = await System.IO.File.ReadAllLinesAsync(@"Day1Input.txt");
+int[]? numberLines = textLineDay1.Select(textLine => Convert.ToInt32(textLine)).ToArray();
 
 int increasedCount = 0;
 
-for (int i = 3; i < textLine.Length; i++)
+for (int i = 3; i < textLineDay1.Length; i++)
 {
     int previousSum = numberLines[i - 1] + numberLines[i - 2] + numberLines[i - 3];
     int currentSum = numberLines[i] + numberLines[i - 1] + numberLines[i - 2];
@@ -14,5 +18,38 @@ for (int i = 3; i < textLine.Length; i++)
     }
 }
 
-Console.WriteLine(increasedCount);
-Console.ReadLine();
+Console.WriteLine("Day 1 Answer: " + increasedCount);
+Console.ReadKey();
+Console.WriteLine();
+
+
+string[]? textLineDay2 = await System.IO.File.ReadAllLinesAsync(@"Day2Input.txt");
+
+int horizontalPosition = 0;
+int depth = 0;
+
+foreach (string line in textLineDay2)
+{
+    if (line.Contains("down"))
+    {
+        depth = depth + Convert.ToInt32(line.Replace("down ", ""));
+    }
+    else if (line.Contains("up"))
+    {
+        depth = depth - Convert.ToInt32(line.Replace("up ", ""));
+    }
+    else if (line.Contains("forward"))
+    {
+        horizontalPosition = horizontalPosition + Convert.ToInt32(line.Replace("forward ", ""));
+    }
+}
+
+Console.WriteLine("================================");
+Console.WriteLine("=============Day 2==============");
+Console.WriteLine("================================");
+
+Console.WriteLine("Forward Sum: " + horizontalPosition);
+Console.WriteLine("Depth Sum: " + depth);
+Console.WriteLine("Multiple: " + (depth * horizontalPosition));
+
+Console.ReadKey();
