bfa1205f22f83c5df5f4c5fda674fe88842b1101
Jacob Jang <jjang@uwm.com>
2021-12-06 16:43:47 -0500

    my progress of part 2

--- AdventOfCode/Day6/Day6Part1.cs
+++ AdventOfCode/Day6/Day6Part1.cs
@@ -10,27 +10,80 @@ namespace AdventOfCode.Day6
     {
         public static async Task Execute()
         {
-            const int days = 80;
+            const int days = 256;
+            const int bucketLimit = 1000000;
             string[] input = await File.ReadAllLinesAsync(@"Day6\Input.txt");
 
-            List<int> allFish = input[0].Split(',').Select(_ => Convert.ToInt32(_)).ToList();
-            int numberOfFish = allFish.Count;
+            // List<int> allFish = input[0].Split(',').Select(_ => Convert.ToInt32(_)).ToList();
+            List<int> allFish = new() { 1 };
+
+            List<List<int>> bucketOfFish = new() { allFish };
 
             for (int i = 1; i <= days; i++)
             {
-                for (int j = 0; j < numberOfFish; j++)
+
+                int numberOfBuckets = bucketOfFish.Count;
+
+                for (int k = 0; k < numberOfBuckets; k++)
                 {
-                    if (allFish[j] == 0)
+                    int numberOfFishInBucket = bucketOfFish[k].Count;
+
+                    List<int> newBucket = new();
+
+                    List<List<int>> bucketsOfBucket = new();
+
+                    for (int j = 0; j < numberOfFishInBucket; j++)
                     {
-                        allFish.Add(8); // Add Fish
-                        allFish[j] = 7; // Reset Fish
+                        if (bucketOfFish[k][j] == 0)
+                        {
+                            if (bucketOfFish[k].Count > bucketLimit)
+                            {
+                                if (newBucket.Count > bucketLimit)
+                                {
+                                    bucketsOfBucket.Add(newBucket);
+                                    newBucket = new();
+                                }
+
+                                newBucket.Add(8);
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    bucketOfFish[k].Add(8); // Add Fish
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine($"Error Count: { bucketOfFish[k].Count}");
+                                    throw;
+                                }
+
+
+                            }
+                            bucketOfFish[k][j] = 7; // Reset Fish
+
+                        }
+                        bucketOfFish[k][j]--;
                     }
-                    allFish[j]--;
+
+                    if (newBucket.Any())
+                    {
+                        bucketOfFish.Add(newBucket);
+                    }
+
+                    if (bucketsOfBucket.Any())
+                    {
+                        bucketOfFish.AddRange(bucketsOfBucket);
+                    }
                 }
-                numberOfFish = allFish.Count;                
+
+                Console.WriteLine($"Day {i}: {bucketOfFish.Count} buckets");
             }
 
-            Console.WriteLine($"Answer: {numberOfFish}");
+
+
+
+            Console.WriteLine($"Answer: {bucketOfFish.Select(_ => _.Count).Sum()}");
         }
     }
 }
---
+++ AdventOfCode/Day6/Day6Part2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day6
+{
+    internal class Day6Part2
+    {
+        public static async Task Execute()
+        {
+            const int days = 80;
+            string[] input = await File.ReadAllLinesAsync(@"Day6\Input.txt");
+
+
+
+        }
+    }
+}
