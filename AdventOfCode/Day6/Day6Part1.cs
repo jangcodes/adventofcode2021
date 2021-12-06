b31529e2403d76570323b44337a94aca704f326d
jangcodes <jacob.jang@outlook.com>
2021-12-06 06:34:53 -0500

    Day 6 prep

---
+++ AdventOfCode/Day6/Day6Part1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day6
+{
+    internal class Day6Part1
+    {
+        public static async Task Execute()
+        {
+            string[] input = await File.ReadAllLinesAsync(@"Day6\Input.txt");
+
+        }
+    }
+}
