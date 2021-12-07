fcfb9503928d4476acef82a83ebf359ebdeb1a7e
jangcodes <jacob.jang@outlook.com>
2021-12-06 20:20:06 -0500

    Revert "trying task?"
    This reverts commit 842e9c36dd70d96c8486ad75b935b62b64c46e22.

--- AdventOfCode/Day6/Day6Part1.cs
+++ AdventOfCode/Day6/Day6Part1.cs
@@ -10,75 +10,72 @@ namespace AdventOfCode.Day6
     {
         public static async Task Execute()
         {
-            const ushort days = 256;
+            const int days = 256;
             const int bucketLimit = 1000000;
             string[] input = await File.ReadAllLinesAsync(@"Day6\Input.txt");
 
             // List<int> allFish = input[0].Split(',').Select(_ => Convert.ToInt32(_)).ToList();
-            List<byte> allFish = new() { 1 };
+            List<int> allFish = new() { 1 };
 
-            List<List<byte>> bucketOfFish = new() { allFish };
+            List<List<int>> bucketOfFish = new() { allFish };
 
             for (int i = 1; i <= days; i++)
             {
 
                 int numberOfBuckets = bucketOfFish.Count;
-                List<Task> tasks = new List<Task>();
+
                 for (int k = 0; k < numberOfBuckets; k++)
                 {
-                    var task = Task.Factory.StartNew(() =>
+                    int numberOfFishInBucket = bucketOfFish[k].Count;
+
+                    List<int> newBucket = new();
+
+                    List<List<int>> bucketsOfBucket = new();
+
+                    for (int j = 0; j < numberOfFishInBucket; j++)
                     {
-                        int numberOfFishInBucket = bucketOfFish[k].Count;
-
-                        List<byte> newBucket = new();
-
-                        List<List<byte>> bucketsOfBucket = new();
-
-                        
-                        for (int j = 0; j < numberOfFishInBucket; j++)
+                        if (bucketOfFish[k][j] == 0)
                         {
-
-
-                            if (bucketOfFish[k][j] == 0)
+                            if (bucketOfFish[k].Count > bucketLimit)
                             {
-                                if (bucketOfFish[k].Count > bucketLimit)
+                                if (newBucket.Count > bucketLimit)
                                 {
-                                    if (newBucket.Count > bucketLimit)
-                                    {
-                                        bucketsOfBucket.Add(newBucket);
-                                        newBucket = new();
-                                    }
-
-                                    newBucket.Add(8);
+                                    bucketsOfBucket.Add(newBucket);
+                                    newBucket = new();
                                 }
-                                else
+
+                                newBucket.Add(8);
+                            }
+                            else
+                            {
+                                try
                                 {
                                     bucketOfFish[k].Add(8); // Add Fish
-
                                 }
-                                bucketOfFish[k][j] = 7; // Reset Fish
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine($"Error Count: { bucketOfFish[k].Count}");
+                                    throw;
+                                }
+
 
                             }
-                            bucketOfFish[k][j]--;
+                            bucketOfFish[k][j] = 7; // Reset Fish
 
-                            
                         }
+                        bucketOfFish[k][j]--;
+                    }
 
-                        if (newBucket.Any())
-                        {
-                            bucketOfFish.Add(newBucket);
-                        }
+                    if (newBucket.Any())
+                    {
+                        bucketOfFish.Add(newBucket);
+                    }
 
-                        if (bucketsOfBucket.Any())
-                        {
-                            bucketOfFish.AddRange(bucketsOfBucket);
-                        }
-                    });
-
-                    tasks.Add(task);
+                    if (bucketsOfBucket.Any())
+                    {
+                        bucketOfFish.AddRange(bucketsOfBucket);
+                    }
                 }
-
-                Task.WaitAll(tasks.ToArray());
 
                 Console.WriteLine($"Day {i}: {bucketOfFish.Count} buckets");
             }
