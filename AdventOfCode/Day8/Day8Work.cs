9f9d24d30fd8dae37c805bea514da40ba2cff2d6
jangcodes <jacob.jang@outlook.com>
2021-12-08 06:45:58 -0500

    Day 8 Part 2 in progress

--- AdventOfCode/Day8/Day8Work.cs
+++ AdventOfCode/Day8/Day8Work.cs
@@ -12,6 +12,7 @@ namespace AdventOfCode.Day8
             string[] input = await File.ReadAllLinesAsync(@"Day8\Input.txt");
 
             Part1(input);
+            Part2(input);
         }
 
         private static void Part1(string[] input)
@@ -23,6 +24,36 @@ namespace AdventOfCode.Day8
                 sum += secondPart.Count(x => x.Length == 2 || x.Length == 3 || x.Length == 4 || x.Length == 7);
             }
             Console.WriteLine($"Answer: {sum}");
+        }
+
+        private static void Part2(string[] input)
+        {
+            foreach (string line in input)
+            {
+                char[] singleDigit = new char[7] { '1', '1', '1', '1', '1', '1', '1' };
+
+                var splitInput = line.Split(" | ");
+
+                var signals = splitInput[0].Split(' ');
+                var findDigitEight = signals.FirstOrDefault(x => x.Length == 7);
+                if (findDigitEight != null)
+                {
+                    singleDigit = findDigitEight.ToArray();
+                }
+
+                DisplayDigit(singleDigit);
+            }
+        }
+
+        private static void DisplayDigit(char[] s)
+        {
+            Console.WriteLine($" {s[0]}{s[0]}{s[0]}{s[0]} ");
+            Console.WriteLine($"{s[1]}    {s[2]}");
+            Console.WriteLine($"{s[1]}    {s[2]}");
+            Console.WriteLine($" {s[3]}{s[3]}{s[3]}{s[3]} ");
+            Console.WriteLine($"{s[4]}    {s[5]}");
+            Console.WriteLine($"{s[4]}    {s[5]}");
+            Console.WriteLine($" {s[6]}{s[6]}{s[6]}{s[6]} ");
         }
     }
 }
