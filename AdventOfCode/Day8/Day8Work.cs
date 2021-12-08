c3c26c46f9260991b86b9fce849685742245bf83
Jacob Jang <jjang@uwm.com>
2021-12-08 12:47:25 -0500

    more compact solution

--- AdventOfCode/Day8/Day8Work.cs
+++ AdventOfCode/Day8/Day8Work.cs
@@ -29,45 +29,33 @@ namespace AdventOfCode.Day8
             foreach (string line in input)
             {
                 var splitInput = line.Split(" | ");
-                var signals = splitInput[0].Split(' ');
+                var signals = splitInput.Select(si => si.Split(' ').Select(i => string.Concat(i.OrderBy(c => c)))).ToArray();
 
                 string[] digits = new string[10];
 
-                digits[1] = signals.Single(x => x.Length == 2);
-                digits[4] = signals.Single(x => x.Length == 4);
-                digits[7] = signals.Single(x => x.Length == 3);
-                digits[8] = signals.Single(x => x.Length == 7);
+                digits[1] = signals[0].Single(x => x.Length == 2); // #1
+                digits[4] = signals[0].Single(x => x.Length == 4); // #4
+                digits[7] = signals[0].Single(x => x.Length == 3); // #7
+                digits[8] = signals[0].Single(x => x.Length == 7); // #8
 
-                digits[6] = GetSixFromOne(signals, digits[1].ToCharArray());
-                digits[9] = GetNineFromFour(signals, digits[4].ToCharArray());
-                digits[0] = signals.Single(x => x.Length == 6 && x != digits[9] && x != digits[6]);
+                var sixNineZero = signals[0].Where(x => x.Length == 6);                         // #6, #9 and #0 have 6 characters
+                digits[6] = sixNineZero.Single(x => digits[1].Count(y => x.Contains(y)) == 1);  // #6 is the only one that shares 1 character with #1
+                digits[9] = sixNineZero.Single(x => digits[4].All(y => x.Contains(y)));         // #9 is the only one that contains all characters from #4
+                digits[0] = sixNineZero.Single(x => x != digits[9] && x != digits[6]);          // If it's not #6 or #9, it is #0
 
-                digits[3] = GetThreeFromOne(signals, digits[1].ToCharArray());
-                digits[5] = GetFiveFromFour(signals.Where(x => x != digits[3]).ToArray(), digits[4].ToCharArray());
-                digits[2] = signals.Single(x => x.Length == 5 && x != digits[3] && x != digits[5]);
-
-                for (int i = 0; i <= 9; i++)
-                {
-                    digits[i] = string.Concat(digits[i].OrderBy(c => c));
-                }
+                var threeFiveTwo = signals[0].Where(x => x.Length == 5);                                            // #3, #5 and #2 have 5 characters
+                digits[3] = threeFiveTwo.Single(x => digits[1].All(y => x.Contains(y)));                            // #3 is the only one that contains all charcter from #1
+                digits[5] = threeFiveTwo.Single(x => x != digits[3] && digits[4].Count(y => x.Contains(y)) == 3);   // Without #3, #5 is the only one that shares 3 character with #4
+                digits[2] = threeFiveTwo.Single(x => x != digits[3] && x != digits[5]);                             // If it's not #3 or #5, it's 2 
 
                 string currentNumber = "";
-                foreach (var item in splitInput[1].Split(' '))
-                {
-                    var sortedString = string.Concat(item.OrderBy(c => c));
-                    var findDigit = digits.ToList().IndexOf(sortedString).ToString();
-                    currentNumber += findDigit;
-                }
+                foreach (var item in signals[1])
+                    currentNumber += Array.IndexOf(digits, item);
 
                 result += Convert.ToInt32(currentNumber);
             }
 
             Console.WriteLine($"Part 2 Answer: {result}");
         }
-
-        private static string GetFiveFromFour(string[] input, char[] four) => input.Single(x => x.Length == 5 && four.Count(y => x.Contains(y)) == 3);
-        private static string GetThreeFromOne(string[] input, char[] one) => input.Single(x => x.Length == 5 && one.All(y => x.Contains(y)));
-        private static string GetSixFromOne(string[] input, char[] one) => input.Single(x => x.Length == 6 && one.Count(y => x.Contains(y)) == 1);
-        private static string GetNineFromFour(string[] input, char[] four) => input.Single(x => x.Length == 6 && four.All(y => x.Contains(y)));
     }
 }
