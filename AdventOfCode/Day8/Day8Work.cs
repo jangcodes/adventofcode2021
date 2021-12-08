56c1a506bd39d31eb6fb1acede52e8d0f6ea2fef
Jacob Jang <jjang@uwm.com>
2021-12-08 09:34:36 -0500

    Day 8 Part 2 Answer and Part 1 refactor

--- AdventOfCode/Day8/Day8Work.cs
+++ AdventOfCode/Day8/Day8Work.cs
@@ -17,43 +17,57 @@ namespace AdventOfCode.Day8
 
         private static void Part1(string[] input)
         {
-            int sum = 0;
-            foreach (string line in input)
-            {
-                var secondPart = line[line.IndexOf('|')..].Split(" ");
-                sum += secondPart.Count(x => x.Length == 2 || x.Length == 3 || x.Length == 4 || x.Length == 7);
-            }
+            int[] listOfNumbers = new int[] { 2, 3, 4, 7 };
+            int sum = input.Select(line => line[line.IndexOf('|')..].Split(" ")).Sum(x => x.Count(x => listOfNumbers.Contains(x.Length)));
             Console.WriteLine($"Answer: {sum}");
         }
 
         private static void Part2(string[] input)
         {
+            int result = 0;
+
             foreach (string line in input)
             {
-                char[] singleDigit = new char[7] { '1', '1', '1', '1', '1', '1', '1' };
-
                 var splitInput = line.Split(" | ");
-
                 var signals = splitInput[0].Split(' ');
-                var findDigitEight = signals.FirstOrDefault(x => x.Length == 7);
-                if (findDigitEight != null)
+
+                string[] digits = new string[10];
+
+                digits[1] = signals.Single(x => x.Length == 2);
+                digits[4] = signals.Single(x => x.Length == 4);
+                digits[7] = signals.Single(x => x.Length == 3);
+                digits[8] = signals.Single(x => x.Length == 7);
+
+                digits[6] = GetSixFromOne(signals, digits[1].ToCharArray());
+                digits[9] = GetNineFromFour(signals, digits[4].ToCharArray());
+                digits[0] = signals.Single(x => x.Length == 6 && x != digits[9] && x != digits[6]);
+
+                digits[3] = GetThreeFromOne(signals, digits[1].ToCharArray());
+                digits[5] = GetFiveFromFour(signals.Where(x => x != digits[3]).ToArray(), digits[4].ToCharArray());
+                digits[2] = signals.Single(x => x.Length == 5 && x != digits[3] && x != digits[5]);
+
+                for (int i = 0; i <= 9; i++)
                 {
-                    singleDigit = findDigitEight.ToArray();
+                    digits[i] = string.Concat(digits[i].OrderBy(c => c));
                 }
 
-                DisplayDigit(singleDigit);
+                string currentNumber = "";
+                foreach (var item in splitInput[1].Split(' '))
+                {
+                    var sortedString = string.Concat(item.OrderBy(c => c));
+                    var findDigit = digits.ToList().IndexOf(sortedString).ToString();
+                    currentNumber += findDigit;
+                }
+
+                result += Convert.ToInt32(currentNumber);
             }
+
+            Console.WriteLine($"Part 2 Answer: {result}");
         }
 
-        private static void DisplayDigit(char[] s)
-        {
-            Console.WriteLine($" {s[0]}{s[0]}{s[0]}{s[0]} ");
-            Console.WriteLine($"{s[1]}    {s[2]}");
-            Console.WriteLine($"{s[1]}    {s[2]}");
-            Console.WriteLine($" {s[3]}{s[3]}{s[3]}{s[3]} ");
-            Console.WriteLine($"{s[4]}    {s[5]}");
-            Console.WriteLine($"{s[4]}    {s[5]}");
-            Console.WriteLine($" {s[6]}{s[6]}{s[6]}{s[6]} ");
-        }
+        private static string GetFiveFromFour(string[] input, char[] four) => input.Single(x => x.Length == 5 && four.Count(y => x.Contains(y)) == 3);
+        private static string GetThreeFromOne(string[] input, char[] one) => input.Single(x => x.Length == 5 && one.All(y => x.Contains(y)));
+        private static string GetSixFromOne(string[] input, char[] one) => input.Single(x => x.Length == 6 && one.Count(y => x.Contains(y)) == 1);
+        private static string GetNineFromFour(string[] input, char[] four) => input.Single(x => x.Length == 6 && four.All(y => x.Contains(y)));
     }
 }
