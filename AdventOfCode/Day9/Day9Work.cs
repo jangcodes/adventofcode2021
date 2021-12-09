761a3491863cfd153efa3087d008e6b5d7352ff6
jangcodes <jacob.jang@outlook.com>
2021-12-09 06:48:45 -0500

    Quick Part 1

--- AdventOfCode/Day9/Day9Work.cs
+++ AdventOfCode/Day9/Day9Work.cs
@@ -16,29 +16,30 @@ namespace AdventOfCode.Day9
             Part2(input);
         }
 
-
         private static void Part1(string[] input)
         {
             int columnTotal = input[0].Length;
             int rowTotal = input.Length;
-
-            List<List<int>> grid = new();
-
-            foreach (var item in input)
-            {
-                grid.Add(item.Select(s => Convert.ToInt32(s)).ToList());
-            }
-
-
             List<int> lowNumbers = new();
-
-            foreach (var row in grid)
+            for (int r = 0; r < rowTotal; r++)
             {
-                foreach (var column in grid)
+                for (int c = 0; c < columnTotal; c++)
                 {
+                    int up = (r > 0) ? input[r - 1][c] - '0' : 10;
+                    int down = (r < rowTotal - 1) ? input[r + 1][c] - '0' : 10;
+                    int left = (c > 0) ? input[r][c - 1] - '0' : 10;
+                    int right = (c < columnTotal - 1) ? input[r][c + 1] - '0' : 10;
+
+                    int currentNumber = input[r][c] - '0';
+
+                    if (currentNumber < up && currentNumber < down && currentNumber < left && currentNumber < right)
+                    {
+                        lowNumbers.Add(currentNumber + 1);
+                    }
                 }
             }
 
+            Console.WriteLine($"Part 1 Answer: {lowNumbers.Sum()}");
         }
 
         private static void Part2(string[] input)
