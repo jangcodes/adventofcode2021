18ea61e276e48fad7a7fb466f0bbf55cbf3e8739
jangcodes <jacob.jang@outlook.com>
2021-12-10 06:37:46 -0500

    solution in progress

--- AdventOfCode/Day10/Day10Work.cs
+++ AdventOfCode/Day10/Day10Work.cs
@@ -9,6 +9,8 @@ namespace AdventOfCode.Day10
 {
     internal class Day10Work
     {
+        private static readonly Dictionary<char, char> CompleteBrackets = new() { { '{', '}' }, { '(', ')' }, { '[', ']' }, { '<', '>' } };
+
         public static async Task Execute()
         {
             string[] input = await File.ReadAllLinesAsync(@"Day10\Example.txt");
@@ -20,6 +22,26 @@ namespace AdventOfCode.Day10
         private static void Part1(string[] input)
         {
 
+        }
+
+        private static void CheckSyntaxRecursive(string line)
+        {
+            Dictionary<char, int> scoresPerChar = new() { { ')', 3 }, { ']', 57 }, { '}', 1197 }, { '>', 25137 } };
+
+            int totalScore = 0;
+
+            var OpenBrackets = CompleteBrackets.Select(c => c.Key);
+            var CloseBrackets = CompleteBrackets.Select(c => c.Value);
+            if (line.Length > 1 && OpenBrackets.Contains(line[0]) && !CloseBrackets.Contains(line[1]))
+            {
+                CheckSyntaxRecursive(line[1..]);
+            }
+
+            var correctClosing = CompleteBrackets[line[0]];
+            if (correctClosing != line[1])
+            {
+
+            }
         }
 
         private static void Part2(string[] input)
