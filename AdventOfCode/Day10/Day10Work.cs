46dd5951b2c4e30f7d8d00cb47ba357c773d5889
jangcodes <jacob.jang@outlook.com>
2021-12-10 06:19:35 -0500

    day 10 prep

---
+++ AdventOfCode/Day10/Day10Work.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day10
+{
+    internal class Day10Work
+    {
+        public static async Task Execute()
+        {
+            string[] input = await File.ReadAllLinesAsync(@"Day10\Example.txt");
+
+            Part1(input);
+            Part2(input);
+        }
+
+        private static void Part1(string[] input)
+        {
+
+        }
+
+        private static void Part2(string[] input)
+        {
+
+        }
+
+    }
+}
