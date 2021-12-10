be26c3911ea6d8a46c26256b58bfb64dd3eedeb6
Jacob Jang <jjang@uwm.com>
2021-12-10 08:24:28 -0500

    Part 2 answer

--- AdventOfCode/Day10/Day10Work.cs
+++ AdventOfCode/Day10/Day10Work.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace AdventOfCode.Day10
@@ -10,71 +9,58 @@ namespace AdventOfCode.Day10
     internal class Day10Work
     {
         private static readonly Dictionary<char, char> CompleteBrackets = new() { { '{', '}' }, { '(', ')' }, { '[', ']' }, { '<', '>' } };
-        private static readonly Dictionary<char, int> scoresPerChar = new() { { ')', 3 }, { ']', 57 }, { '}', 1197 }, { '>', 25137 } };
+        private static readonly Dictionary<char, int> IllegalCharScore = new() { { ')', 3 }, { ']', 57 }, { '}', 1197 }, { '>', 25137 } };
+        private static readonly Dictionary<char, int> IncompleteCharScores = new() { { '(', 1 }, { '[', 2 }, { '{', 3 }, { '<', 4 } };
 
         public static async Task Execute()
         {
             string[] input = await File.ReadAllLinesAsync(@"Day10\Input.txt");
 
-            var incompleteList = Part1(input);
-            Part2(incompleteList);
-        }
+            int totalIllegalCharScore = 0;
+            List<long> incompleteScoreList = new();
 
-        private static List<string> Part1(string[] input)
-        {
-            List<string> incomplete = new();
-
-            int sum = 0;
             foreach (var line in input)
             {
-                int illegalCharScore = CheckSyntax(line);
+                int illegalCharScore = 0;
+                
+                var OpenBrackets = CompleteBrackets.Select(c => c.Key);
+                var CloseBrackets = CompleteBrackets.Select(c => c.Value);
+
+                List<char> cleanedUpLine = new();
+                foreach (var c in line)
+                {
+                    if (OpenBrackets.Contains(c))
+                    {
+                        cleanedUpLine.Add(c);
+                    }
+                    else if (CloseBrackets.Contains(c))
+                    {
+                        if (CompleteBrackets[cleanedUpLine.Last()] != c)
+                        {
+                            illegalCharScore = IllegalCharScore[c];
+                            totalIllegalCharScore += illegalCharScore;
+                            break;
+                        }
+                        cleanedUpLine.RemoveAt(cleanedUpLine.Count - 1);
+                    }
+                }
 
                 if (illegalCharScore == 0)
                 {
-                    incomplete.Add(line);
-                }
-                else
-                {
-                    sum += illegalCharScore;
-                }
-            }
+                    long incompleteScore = 0;
+                    cleanedUpLine.Reverse();
 
-            Console.WriteLine($"Part 1 Answer: {sum}");
+                    foreach (var c in cleanedUpLine)
+                        incompleteScore = (incompleteScore * 5) + IncompleteCharScores[c];
 
-            return incomplete;
-        }
-
-        private static int CheckSyntax(string line)
-        {
-            var OpenBrackets = CompleteBrackets.Select(c => c.Key);
-            var CloseBrackets = CompleteBrackets.Select(c => c.Value);
-
-            List<char> tempSolution = new();
-            foreach (var c in line)
-            {
-                if (OpenBrackets.Contains(c))
-                {
-                    tempSolution.Add(c);
-                }
-                else if (CloseBrackets.Contains(c))
-                {
-                    if (CompleteBrackets[tempSolution.Last()] == c)
-                    {
-                        tempSolution.RemoveAt(tempSolution.Count - 1);
-                    }
-                    else
-                    {
-                        return scoresPerChar[c];
-                    }
+                    incompleteScoreList.Add(incompleteScore);
                 }
             }
 
-            return 0;
-        }
+            incompleteScoreList.Sort();            
 
-        private static void Part2(List<string> input)
-        {
-
+            Console.WriteLine($"Part 1 Answer: {totalIllegalCharScore}");
+            Console.WriteLine($"Part 2 Answer: {incompleteScoreList[incompleteScoreList.Count / 2]}");
         }
     }
 }
