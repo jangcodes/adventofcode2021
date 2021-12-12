cfa065dd1247ade0353cfb56e1748adabdd05356
jangcodes <jacob.jang@outlook.com>
2021-12-12 13:42:43 -0500

    Part 1 Answer

--- AdventOfCode/Week2/Day12/Day12Work.cs
+++ AdventOfCode/Week2/Day12/Day12Work.cs
@@ -9,9 +9,44 @@ namespace AdventOfCode.Week2.Day12
 {
     internal class Day12Work
     {
+        private static List<string[]> InputNoStart = new();
+
+        private static int GlobalCounter = 0;
+
         public static async Task Execute()
         {
-            string[] input = await File.ReadAllLinesAsync(@"Week2\Day12\Example1.txt");
+            string[] input = await File.ReadAllLinesAsync(@"Week2\Day12\Input.txt");
+
+            List<string[]> inputList = input.Select(x => x.Split('-').ToArray()).ToList();
+            InputNoStart = inputList.Where(x => !x.Contains("start")).ToList();
+
+            var startPoints = inputList.Where(x => x.Contains("start"));
+            foreach(var startPoint in startPoints)
+            {
+                string firstPoint = startPoint.First(_ => _ != "start");
+                MyRecursiveFunction(firstPoint, new string[] { firstPoint });
+            }
+
+            Console.WriteLine($"Part 1 Answer: {GlobalCounter}");
+        }
+
+        private static void MyRecursiveFunction(string caveName, string[] currentPath)
+        {
+            var nextCaves = InputNoStart.Where(x => x.Contains(caveName));
+
+            foreach (var cave in nextCaves)
+            {
+                var nextPoint = cave.First(_ => _ != caveName);
+
+                if (nextPoint == "end")
+                {
+                    GlobalCounter++;
+                }
+                else if (nextPoint.All(_ => char.IsUpper(_)) || !currentPath.Contains(nextPoint))
+                {
+                    MyRecursiveFunction(nextPoint, currentPath.Append(nextPoint).ToArray());
+                }
+             }
         }
     }
 }
