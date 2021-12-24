4053836f639c3568ebb3f03017322a6cb71d91a6
jangcodes <jacob.jang@outlook.com>
2021-12-24 00:02:49 -0500

    Part 1 answer

--- AdventOfCode/Week3/Day17/Day17Work.cs
+++ AdventOfCode/Week3/Day17/Day17Work.cs
@@ -12,6 +12,21 @@ namespace AdventOfCode.Week3.Day17
         public static async Task Execute()
         {
             string[] input = await File.ReadAllLinesAsync(@"Week3\Day17\Input.txt");
+
+            var xCoordinates = input[0].Replace("x=", "").Split("..");
+            var yCoordinates = input[1].Replace("y=","").Split("..").Select(x => Convert.ToInt32(x)).ToArray();
+
+
+            (int y, int x) startingPoint = new(0, 0);
+
+
+            var n = Math.Abs(yCoordinates[0]) - 1;
+
+            var result = (n * (n + 1)) / 2;
+
+            Console.WriteLine($"Part 1 Answer: {result}");
+
+
         }
     }
 }
